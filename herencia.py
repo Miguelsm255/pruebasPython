0122584a956d69bf56d4091971352465e3189677
Miguelsm255 <mikesm256@gmail.com>
2021-04-02 23:01:44 +0200

    class 'vehiculo' definida

--- herencia.py
+++ herencia.py
@@ -8,3 +8,17 @@ class Vehiculos():
         self.acelera = False
         self.frena = False
 
+    def arrancar(self):
+        self.enmarcha = True
+
+    def acelerar(self):
+        self.acelera = True
+    
+    def frenar(self):
+        self.frena = True
+    
+    def estado(self):
+        print ('Marca:', self.marca, '\nModelo:', self.modelo, '\nEn marcha:', self.enmarcha,
+            '\nAcelerando:', self.acelera, '\nFrenando:', self.frena)
+        
+
