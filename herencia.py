2bc2eaf7aec2d7e1dc944928756c26647d05af96
Miguelsm255 <mikesm256@gmail.com>
2021-04-02 22:49:42 +0200

    def __init__() de 'herencia'

---
+++ herencia.py
@@ -0,0 +1,10 @@
+class Vehiculos():
+
+    def __init__(self,marca,modelo):
+
+        self.marca = marca
+        self.modelo = modelo
+        self.enmarcha = False
+        self.acelera = False
+        self.frena = False
+
