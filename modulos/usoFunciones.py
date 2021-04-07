31cfc03ca0f42febb635ca2eda3c95e22a57f516
Miguelsm255 <mikesm256@gmail.com>
2021-04-07 18:35:04 +0200

    usoFunciones fuera de carpeta 'modulos'

---
+++ modulos/usoFunciones.py
@@ -0,0 +1,8 @@
+from funcionesMatematicas import *
+
+print(sumar(1,3))
+
+print(restar(9,3))
+
+print(multiplicar(7,3))
+
--- usoFunciones.py
+++
@@ -1,7 +0,0 @@
-from modulos.funcionesMatematicas import *
-
-print(sumar(1,3))
-
-print(restar(9,3))
-
-print(multiplicar(7,3))
